85f4976bf68848d22fe43f7041036b269b9ecbcf
Eo Jin Kim <eooojean@gmail.com>
2026-07-19 10:33:03 -0500

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1m = sorted(s1)
+        hs = set([s for s in s1])
+        for i in range(len(s2)-len(s1m)+1):
+            if s2[i] in hs:
+                new = sorted(s2[i:i+len(s1)])
+                if new == s1m:
+                    return True
+        return False
+
+            
