9eea72a727924140bb833c010931a7d310864302
Eo Jin Kim <eooojean@gmail.com>
2026-09-10 21:30:14 -0500

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, key: int, val: int):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.dict = {}
+        self.capacity = capacity
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node: Node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _insert(self, node: Node):
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev.next = node
+        self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key in self.dict:
+            node = self.dict[key]
+            self._remove(node)
+            self._insert(node)
+            return node.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dict:
+            self._remove(self.dict[key])
+        node = Node(key, value)
+        self.dict[key] = node
+        self._insert(node)
+
+        if len(self.dict) > self.capacity:
+            lru = self.head.next
+            self._remove(lru)
+            del self.dict[lru.key]
