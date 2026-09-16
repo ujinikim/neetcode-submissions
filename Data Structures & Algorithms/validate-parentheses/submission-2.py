4bcd376a99b1d9b4b524c56300961054e547e866
Eo Jin Kim <eooojean@gmail.com>
2026-09-16 00:49:20 -0500

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+        pair = {')': '(', '}': '{', ']': '['}
+
+        for ch in s:
+            if ch not in pair:
+                
+                st.append(ch)
+            else:
+                if not len(st):
+                    return False
+                
+                if pair[ch] != st[-1]:
+                    return False
+                else:
+                    st.pop()
+
+        
+        return False if len(st) else True
